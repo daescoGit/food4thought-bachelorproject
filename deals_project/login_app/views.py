7e68d94388473626e79fbaab6bce412a1156b0ba
dan <daneskildsen1234@gmail.com>
2020-12-20 16:04:20 +0100

    account controls

--- deals_project/login_app/views.py
+++ deals_project/login_app/views.py
@@ -10,18 +10,14 @@ from . messaging import email_message
 
 
 def login(request):
-    context = {}
-
     if request.method == "POST":
         user = authenticate(request, username=request.POST['user'], password=request.POST['password'])
         if user:
             dj_login(request, user)
             return HttpResponseRedirect(reverse('deals_app:base'))
         else:
-            context = {
-                'error': 'Bad username or password.'
-            }
-    return render(request, 'login_app/login.html', context)
+            messages.error(request, "Invalid username or password, please try again.") 
+    return render(request, 'login_app/login.html')
 
 
 def logout(request):
@@ -32,27 +28,31 @@ def logout(request):
 def request_password_reset(request):
     if request.method == "POST":
         user = None
-        post_user = request.POST['email']
-        try:
-            user = User.objects.get(email=post_user)
-        except: 
-            messages.success(request, f"An email will be sent to {request.POST['email']} if an account exists. ") 
-            print(f"Invalid password request: {post_user}")
-            return HttpResponseRedirect(reverse('login_app:password_reset'))
+        email = request.POST['email']
+        if email != '':
+            try:
+                user = User.objects.get(email=email)
+            except: 
+                messages.success(request, f"An email will be sent to {request.POST['email']} if an account exists. ") 
+                print(f"Invalid password request: {email}")
+                return HttpResponseRedirect(reverse('login_app:password_reset'))
 
-        if user:
-            prr = PasswordResetRequest()
-            prr.user = user
-            prr.save()
- 
-            django_rq.enqueue(email_message, {
-               'token' : prr.token,
-               'email' : prr.user.email,
-            })
-            #Print(prr)
-            messages.success(request, f"An email will be sent to {prr.user.email} if an account exists. ") 
-            print(f"Valid password request: {post_user}")
-            return HttpResponseRedirect(reverse('login_app:password_reset'))
+            if user:
+                prr = PasswordResetRequest()
+                prr.user = user
+                prr.save()
+    
+                django_rq.enqueue(email_message, {
+                'token' : prr.token,
+                'email' : prr.user.email,
+                })
+                #Print(prr)
+                messages.success(request, f"An email will be sent to {prr.user.email} if an account exists. ") 
+                print(f"Valid password request: {email}")
+                return HttpResponseRedirect(reverse('login_app:password_reset'))
+        else:
+            messages.error(request, "Please enter a valid email.") 
+            print(f"Invalid email: {email}")
 
     return render(request, 'login_app/request_password_reset.html')
 
@@ -70,18 +70,17 @@ def password_reset(request):
                 prr.save()
             except:
                 print("Invalid password reset attempt.")
-                messages.error(request, f"Invalid password reset attempt.") 
+                messages.error(request, "Invalid password reset attempt.") 
                 return render(request, 'login_app/password_reset.html')
                 
             user = prr.user
             user.set_password(password)
             user.save()
-            messages.success(request, f"Password successfully reset.") 
+            messages.success(request, "Password successfully reset.") 
             return HttpResponseRedirect(reverse('login_app:login'))
         else:
             print("Passwords not matching.")
-            messages.error(request, f"Passwords did not match.") 
-            return render(request, 'login_app/password_reset.html')
+            messages.error(request, "Passwords did not match.") 
 
     return render(request, 'login_app/password_reset.html')
 
@@ -89,34 +88,37 @@ def password_reset(request):
 @login_required
 def password_change(request):
     if request.method == "POST":
-        old_password = request.POST['old_password']
         new_password = request.POST['new_password']
-
-        if old_password != new_password:
-            try:
-                user = Users.objects.get(user=request.user)
-            except:
-                print("Invalid password reset attempt.")
-                messages.error(request, f"Invalid password reset attempt.") 
-                return HttpResponseRedirect(reverse('login_app:edit_account'))
-                
+        confirm_password = request.POST['confirm_password']
+        # require strong password for production
+        if new_password == confirm_password and request.user.is_authenticated: 
+            user = request.user  
             user.set_password(new_password)
             user.save()
             messages.success(request, "Password successfully changed.") 
-            return HttpResponseRedirect(reverse('login_app:edit_account'))
         else:
-            print("Same passwords.")
-            messages.error(request, "Password cannot be the same as the current.") 
-            return HttpResponseRedirect(reverse('login_app:edit_account'))
+            print("Authentication error / password did not match.")
+            messages.error(request, "The entered passwords did not match.") 
+    
+    return HttpResponseRedirect(reverse('login_app:edit_account'))
 
 
 @login_required
 def email_change(request):
+    if request.method == "POST":
+        new_email = request.POST['new_email']
+        if request.user.is_authenticated: 
+            user = request.user  
+            user.email = new_email
+            user.save()
+            messages.success(request, "Email successfully changed.") 
+        else:
+            print("Authentication error.")
+    
     return HttpResponseRedirect(reverse('login_app:edit_account'))
 
 
 def register(request):
-    context = {}
     if request.method == "POST":
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
@@ -124,23 +126,17 @@ def register(request):
         email = request.POST['email']
         if password == confirm_password:
             if User.objects.filter(username=user_name).exists():
-                context = {
-                    'error': 'User already exists'
-                }
-                return render(request, 'login_app/register.html', context)
+                messages.error(request, "User already exists, please try another name.") 
 
-            if User.objects.create_user(user_name, email, password):
+            elif User.objects.create_user(user_name, email, password):
+                messages.success(request, "Account sucessfully created.") 
                 return HttpResponseRedirect(reverse('login_app:login'))
             else:
-                context = {
-                    'error': 'Could not create user account - please try again.'
-                }
+                messages.error(request, "Could not create account, please try again.") 
         else:
-            context = {
-                'error': 'Passwords did not match. Please try again.'
-            }
+            messages.error(request, "Passwords did not match, please try again.") 
 
-    return render(request, 'login_app/register.html', context)
+    return render(request, 'login_app/register.html')
 
 
 @login_required
